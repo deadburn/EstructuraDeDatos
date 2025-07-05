5e0036ea6d187a076f92f229bb1f1bc99244249f
Gabriel Rangel <rangelmavo@gmail.com>
2025-07-04 21:01:38 -0500

    v.1.0
    Creacion de los metodos guardarNumero, incluirLista, insertarDato(Por corregir)

---
+++ Formulario.py
@@ -0,0 +1,32 @@
+from lista_datos import Lista_numeros
+#funciones
+objLista=Lista_numeros()
+
+class Calculadora:
+    
+    def pedir_numero(self):
+        numero1= input("Numero 1 ")
+        numero2= input("Numero 2 ")
+        return numero1, numero2
+    
+    def almacenar_numero(self, datonum1, datonum2):
+        dato_numero =[[datonum1, datonum2]]
+        #objLista.guardar_numero(dato_numero)
+        #objLista.incluir_lista(dato_numero)
+        posicion =input("Digite la posicion del dato ")
+        objLista.insertar_dato(dato_numero, posicion) 
+        
+#Codigo Principal
+
+objCalculadora=Calculadora()
+
+auxnum1, auxnum2=objCalculadora.pedir_numero() 
+objCalculadora.almacenar_numero(auxnum1,auxnum2)
+
+auxnum1, auxnum2=objCalculadora.pedir_numero() 
+objCalculadora.almacenar_numero(auxnum1,auxnum2)
+
+auxnum1, auxnum2=objCalculadora.pedir_numero() 
+
+
+
---
+++ lista_datos.py
@@ -0,0 +1,22 @@
+class Lista_numeros:
+    def __init__(self):
+        self.lista_numero=[]
+        
+    def guardar_numero(self, dato_numero):
+        self.lista_numero.append(dato_numero)
+        print(self.lista_numero)
+        
+    def incluir_lista(self, dato_numero):
+        self.lista_numero.extend(dato_numero)
+        print(self.lista_numero)
+        
+    def insertar_dato(self,posicion, dato_numero):
+        self.lista_numero.insert(2,5)
+        print(dato_numero + [self.lista_numero])
+    
+    def eliminar_dato(self):
+        pass
+    
+    def ver_Numero(self):
+        pass
+    
